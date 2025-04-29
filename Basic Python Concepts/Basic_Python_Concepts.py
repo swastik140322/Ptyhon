b61d76565912de72f8b505675c44e0fa577ff0ad
swastik140322 <123539536+swastik140322@users.noreply.github.com>
2025-04-29 13:38:24 +0530

    Update Basic_Python_Concepts.py

--- Basic Python Concepts/Basic_Python_Concepts.py
+++ Basic Python Concepts/Basic_Python_Concepts.py
@@ -1,1 +1,16 @@
+# Python Assignment written below for below Tasks 
+#Task 1: Perform Basic Mathematical Operations
+a=input('Enter the first number :')
+b=input('Enter the seccond number : ')
+a=int(a)
+b=int(b)
+print('Addition:  ', a+b)
+print('Subtraction:  ', a-b)
+print('Multiplication:  ', a*b)
+print('Division:  ', a/b)
 
+#Task 2: 
+#Create a Personalized Greeting
+a=input('Enter the first name :')
+b=input('Enter the last name : ')
+print("Hello, " + a +" "+b + "! Welcome to the Python program")
